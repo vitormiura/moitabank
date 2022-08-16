af0d77381d08a9e77c6fa92c62a2029035aee7b1
vitormiura <vitormiura3@gmail.com>
2022-08-16 11:21:31 -0300

    Organize dirs and add navbar component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,9 @@
+import Navbar from '../components/Navbar';
+
 function App() {
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-      Install & Setup Vite + React + Typescript + Tailwind CSS 3
-      </h1>
-    </div>
+    <Navbar/>
   );
 }
 

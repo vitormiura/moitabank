7afe2fe3e79fae1b566e7686373397f526aa10be
vitormiura <vitormiura3@gmail.com>
2022-09-29 11:31:37 -0300

    📦 NEW: update api models

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,3 +1,4 @@
+from unicodedata import decimal
 from django.db import models
 
 class Client(models.Model):
@@ -12,16 +13,16 @@ class Client(models.Model):
     ]
 
     name = models.CharField(max_length=111, null=False)
+    cpf = models.CharField(max_length=14, unique=True, null=False)
+    password = models.CharField(max_length=16, null=False)
+    is_active = models.BooleanField()
     birth_date = models.DateField()
     cnpj = models.CharField(max_length=18)
-    empregado = models.BooleanField()
+    is_employed = models.BooleanField()
     gender = models.CharField(max_length=1, choices=Gender, default=Undefined) 
-
-class User(models.Model):
-    cpf = models.CharField(max_length=14, unique=True, null=False)
-    password = models.CharField(max_length=16)
-    is_active = models.BooleanField()
-    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.content
 
 class Address(models.Model):
     country = models.CharField(max_length=55)
@@ -34,3 +35,50 @@ class Address(models.Model):
     zip_code = models.CharField(max_length=30)
     client = models.ForeignKey(Client, on_delete=models.PROTECT)
 
+class Contacts(models.Model):
+    telephone = models.Charfield(max_lenght=15)
+    email = models.EmailField()
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+
+class Favorites(models.Model):
+    client = models.ManyToManyField(Client, related_name='favorite_invites', on_delete=models.PROTECT, null=False)
+    client_favorite = models.ManyToManyField(Client, related_name='favorites', null=False, on_delete=models.PROTECT)
+
+class Account(models.Model):
+    CHECKING = 'C'
+    SAVING = 'S'
+    WAGE = 'W'
+
+    ACCOUNT_TYPE = [
+        (CHECKING, 'Checking Account'),
+        (SAVING, 'Saving Account'),
+        (WAGE, 'Wage Account'),
+    ]
+
+    balance = models.DecimalField(decimal_places = 2, null=0)
+    salary = models.DecimalField(decimal_places=2)
+    number = models.PositiveIntegerField()
+    agency = models.PositiveIntegerField()
+    type = models.CharField(max_lenght=1, choices=ACCOUNT_TYPE)
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+
+class Loan(models.Model):
+    APPROVED = 'A'
+    DENIED = 'D'
+    PAYING = 'P'
+    WAITING = 'W'
+
+    CONDITION = [
+        (APPROVED, 'Approved'),
+        (DENIED, 'Denied'),
+        (PAYING, 'Paying'),
+        (WAITING, 'Waiting'),
+    ]
+
+    value = models.DecimalField(decimal_places=2)
+    value_fees = models.DecimalField(decimal_place=2)
+    date = models.DateTimeField(auto_now_add=True)
+    fee = models.DecimalField(decimal_places=2)
+    expiration = models.DateField()
+    condition = models.CharField(max_lenght=1, choices=CONDITION, default=WAITING)
+    account = models.ManyToOneRel(Account, related_name='Loan', on_delete=models.PROTECT)

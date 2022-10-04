7a5aa0f05772b1e06124e4c7d938c1904c810785
vitormiura <vitormiura3@gmail.com>
2022-10-04 08:42:29 -0300

    Init auth system

---
+++ api/core/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# admin.site.register(User)
+@admin.register(User)
+class Admin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email"),
+            },
+        ),
+    )

0671e273bf252ac967071b3465b514f4f7d82d36
vitormiura <vitormiura3@gmail.com>
2022-10-31 10:30:47 -0300

    📦 NEW: Add api views

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
+
 from rest_framework import viewsets
-from .models import Client
-from .serializers import ClientSerializer
+from .models import Client, Card, Address, Contacts, Account, Loan, Transaction, BankStatement, LoanPayment
+from .serializers import ClientSerializer, CardSerializer, AddressSerializer, ContactsSerializer, AccountSerializer, LoanPaySerializer, LoanSerializer, TransactionSerializer, BankStateSerializer
 
 class Client(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+class Card(viewsets.ModelViewSet):
+    query = Card.objects.all()
+    serializer_class = CardSerializer
+
+class Address(viewsets.ModelViewSet):
+    query = Address.objects.all()
+    serializer_class = AddressSerializer
+
+class Contacts(viewsets.ModelViewSet):
+    query = Contacts.objects.all()
+    serializer_class = ContactsSerializer
+
+class Accounts(viewsets.ModelViewSet):
+    query = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class Contacts(viewsets.ModelViewSet):
+    query = Contacts.objects.all()
+    serializer_class = ContactsSerializer
+
+class Loan(viewsets.ModelViewSet):
+    query = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+class LoanPayment(viewsets.ModelViewSet):
+    query = LoanPayment.objects.all()
+    serializer_class = LoanPaySerializer
+
+class Transaction(viewsets.ModelViewSet):
+    query = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class BankStatement(viewsets.ModelViewSet):
+    query = BankStatement.objects.all()
+    serializer_class = BankStateSerializer

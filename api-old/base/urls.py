118e0feca9786a96f847e805c11890bda6745e01
vitormiura <vitormiura3@gmail.com>
2022-11-16 10:24:40 -0300

    ‼️ BREAKING: init new proj. to re-do api auth

---
+++ api-old/base/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bank/', include('accounts.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+]

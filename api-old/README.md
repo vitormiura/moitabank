d0913af043eaf9e66b0d63b73ad7f9ffd0036f1d
vitormiura <vitormiura3@gmail.com>
2022-11-22 21:58:47 -0300

    improve: delete old app version

--- api-old/README.md
+++
@@ -1,2 +0,0 @@
-# Moitabank api
-
--- api-old/core/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)

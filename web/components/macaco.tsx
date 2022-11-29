ba4058c748690d428f9cebe4f77fa9f9847fa6ce
vitormiura <vitormiura3@gmail.com>
2022-11-29 08:20:42 -0300

    fix/new: add some sidebar component, delete unused

--- web/components/macaco.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function macaco() {
-  return (
-    <div>macaco</div>
-  )
-}
--- web/pages/client.tsx
+++ web/pages/client.tsx
@@ -20,18 +20,9 @@ export default function Client() {
         setAuth(true);
         if (user.detail == "Unauthenticated!") {
           setAuth(false);
-          setTimeout(async() => {
+          setTimeout(async () => {
             await router.push("/login");
-          }, 5000)
-          return (<div><input type="checkbox" id="my-modal-4" className="modal-toggle" />
-          <label htmlFor="my-modal-4" className="modal cursor-pointer">
-            <label className="modal-box relative" htmlFor="">
-              <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-              <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-            </label>
-          </label>
-          </div>)
-  
+          }, 1000);
         }
       } catch (e) {
         setAuth(false);
@@ -42,18 +33,6 @@ export default function Client() {
 
   return (
     <>
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-        </label>
-      </label>
       <Layout auth={auth}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-screen">
           <h3 className="text-lg font-bold">Welcome {user}!</h3>

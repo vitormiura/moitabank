a9ca361eaf33151c544d02ab0d27f64631c06e92
vitormiura <vitormiura3@gmail.com>
2022-12-02 11:27:11 -0300

    new: add credit card to client page

--- web/pages/client.tsx
+++ web/pages/client.tsx
@@ -7,6 +7,7 @@ export default function Client() {
   const router = useRouter();
 
   const [data, setData] = useState(Object);
+  const [card, setCard] = useState(Object);
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState("");
 
@@ -22,6 +23,9 @@ export default function Client() {
         setAuth(true);
         setTimeout(() => {
           acc(user.id);
+        }, 250);
+        setTimeout(() => {
+          cc(user.id);
         }, 250);
 
         if (user.detail == "Unauthenticated!") {
@@ -46,6 +50,19 @@ export default function Client() {
       })
         .then((e) => e.json())
         .then((e) => setData(e));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function cc(idParam: number) {
+    try {
+      fetch(`http://localhost:8000/bank/card/${idParam}`, {
+        credentials: "include",
+        method: "GET",
+      })
+        .then((e) => e.json())
+        .then((e) => setCard(e));
     } catch (e) {
       console.log(e);
     }
@@ -85,6 +102,51 @@ export default function Client() {
               <div className="text-right">
                 <p className="text-2xl">R$ {data.balance}</p>
                 <p>Balance</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="pt-10">
+            <div className="w-96 h-56 m-auto bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
+              <img
+                className="relative object-cover w-full0 h-full rounded-xl"
+                src="https://i.imgur.com/kGkSg1v.png"
+              />
+
+              <div className="w-full px-8 absolute top-8">
+                <div className="flex justify-between">
+                  <div className="">
+                    <p className="font-light">Name</p>
+                    <p className="font-medium tracking-widest">{user}</p>
+                  </div>
+                  <img
+                    className="w-14 h-14"
+                    src="https://i.imgur.com/bbPHJVe.png"
+                  />
+                </div>
+                <div className="pt-1">
+                  <p className="font-light">Card Number</p>
+                  <p className="font-medium tracking-more-wider">
+                    {card.number}
+                  </p>
+                </div>
+                <div className="pt-6 pr-6">
+                  <div className="flex justify-between">
+                    <div className="">
+                      <p className="font-light text-xs">Expiry</p>
+                      <p className="font-medium tracking-wider text-sm">
+                        {card.expiration_date}
+                      </p>
+                    </div>
+
+                    <div className="">
+                      <p className="font-light text-xs">CVV</p>
+                      <p className="font-bold tracking-more-wider text-sm">
+                        {card.cvv}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
